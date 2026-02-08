fb5b43b51da47c160c57d6403376badaf12ba2f2
Pavlo Yakymenko <world9online@gmail.com>
2026-02-08 18:58:15 +0100

    v0.0.1
    Add modal initialization and event listeners

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,1 +1,36 @@
+// modal.js
+export default function initModal() {
+    // ============================================================================== simple modal
+    const btnmodal = document.querySelector('.open-modal')
+    const modal = document.querySelector('.modal')
+    const dialog = document.querySelector('.modal-dialog')
+    const body = document.querySelector('body')
 
+    btnmodal.addEventListener('click', () => {
+        body.classList.toggle('overflow-hidden')
+        modal.classList.toggle('opacity-0')
+        modal.classList.toggle('pointer-events-none')
+        dialog.classList.toggle('scale-95')
+        dialog.classList.toggle('scale-100')
+    })
+    
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+        body.classList.toggle('overflow-hidden')
+        modal.classList.toggle('opacity-0')
+        modal.classList.toggle('pointer-events-none')
+        dialog.classList.toggle('scale-95')
+        dialog.classList.toggle('scale-100')
+        }
+    })
+    
+    document.addEventListener('keydown', (event) => {
+        if (event.key === "Escape") {
+            body.classList.toggle('overflow-hidden')
+            modal.classList.toggle('opacity-0')
+            modal.classList.toggle('pointer-events-none')
+            dialog.classList.toggle('scale-95')
+            dialog.classList.toggle('scale-100')
+        }
+    })
+}
